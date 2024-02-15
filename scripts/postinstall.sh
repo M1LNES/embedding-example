babac6d129556ede6b1c3305e3aa2d0509f5987a
Milan Janoch <milan.janoch@emplifi.io>
2024-02-15 23:01:26 +0100

    zkouska

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 
 mkdir -p assets # ensure the assets directory exists
-ACCESS_TOKEN=$(cat .env | grep ACCESS_TOKEN | cut -d '=' -f2-)
-PACKAGE_URL=$(cat .env | grep PACKAGE_URL | cut -d '=' -f2- | sed 's/\"//g')
+export ACCESS_TOKEN=$(cat .env | grep ACCESS_TOKEN | cut -d '=' -f2-)
+export PACKAGE_URL=$(cat .env | grep PACKAGE_URL | cut -d '=' -f2- | sed 's/\"//g')
 curl -H "Authorization: Bearer ${ACCESS_TOKEN}" -o ./assets/embedding.js ${PACKAGE_URL}

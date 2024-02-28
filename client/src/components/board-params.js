26c4b362b4c67e7692eeff534200b59e4d318044
Milan Janoch <milan.janoch@emplifi.io>
2024-02-28 16:53:18 +0100

    Finally valid params

--- client/src/components/board-params.js
+++ client/src/components/board-params.js
@@ -162,31 +162,22 @@ export const board_avg = [
 	{
 		value: [
 			{
-				value: '104417270954942',
-				title: 'Salomon Store München',
-				picture:
-					'https://scontent-den2-1.xx.fbcdn.net/v/t39.30808-1/306809571_999501578113169_5573128477611025528_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=4da83f&_nc_ohc=xoJCBqOCex0AX_TD2iW&_nc_ht=scontent-den2-1.xx&edm=AJdBtusEAAAA&oh=00_AfBypyirP9WUSmgYGuTkw7E8v7X7oobqAojD2NBOS6JnWA&oe=65E387F6',
-				link: 'https://www.facebook.com/salomonmuenchen',
-				network: 'facebook',
-				screenName: 'salomonmuenchen',
-			},
-			{
-				value: '111348073619478',
-				title: 'Salomon Store Barcelona',
-				picture:
-					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/307039408_792605865493692_6639290099551662800_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=4da83f&_nc_ohc=jI0gJYxmYMAAX9tF9aR&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfD40q75rf2q1GNI1LKpI00FP_68fk-Dc2D85REBBg-5DQ&oe=65E3EF63',
-				link: 'https://www.facebook.com/salomonstorebarcelona',
-				network: 'facebook',
-				screenName: 'salomonstorebarcelona',
-			},
-			{
 				value: '381443886037593',
 				title: 'Salomon Store Madrid',
 				picture:
-					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/356051315_980815946596969_583284026175324862_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=4da83f&_nc_ohc=qPRImxVoAv8AX-BOzRN&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfDlGNuPs6RfKi7J6YV07XBMg-G2N-pOP1znViz474Pu3g&oe=65E3EEDF',
+					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/356051315_980815946596969_583284026175324862_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=4da83f&_nc_ohc=qPRImxVoAv8AX-evg-a&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfBBIKX57lo7pHZhJqCcotbylqBaNSCOOqcu4uK3MNdqHg&oe=65E3EEDF',
 				link: 'https://www.facebook.com/SalomonStoreMadrid',
 				network: 'facebook',
 				screenName: 'SalomonStoreMadrid',
+			},
+			{
+				value: '235862609914838',
+				title: 'Salomon Hellas',
+				picture:
+					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/306277032_2190414877792925_1266017756509046562_n.jpg?stp=dst-jpg_p200x200&_nc_cat=100&ccb=1-7&_nc_sid=4da83f&_nc_ohc=g547mBkBdCQAX8v8cHK&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfD5gyIMEeg8BClRvs3_p8KuMNAcbrWr7Q0eZwBcAsji7w&oe=65E3854C',
+				link: 'https://www.facebook.com/salomonhellas',
+				network: 'facebook',
+				screenName: 'salomonhellas',
 			},
 			{
 				value: '257484707608796',
@@ -196,6 +187,33 @@ export const board_avg = [
 				link: 'https://www.facebook.com/salomonsouthafrica',
 				network: 'facebook',
 				screenName: 'salomonsouthafrica',
+			},
+			{
+				value: '228378897804',
+				title: 'Salomon Argentina',
+				picture:
+					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/298773717_10159051231452805_5650694120826884829_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=4da83f&_nc_eui2=AeGxCzORNdau-y9v6HyNUJyOBSEUXQsIBXkFIRRdCwgFeXMqmOYxKVhJLQdLlucCW-ppJDyptfHt7pMHmeFPAlGy&_nc_ohc=UkBDvJtxxYUAX8cz_XF&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfABL9n7kFeyEu_i-pSxYSBRJEzGynuVkeraS84AdGVUrQ&oe=65E3717C',
+				link: 'https://www.facebook.com/SalomonArgentinaOficial',
+				network: 'facebook',
+				screenName: 'SalomonArgentinaOficial',
+			},
+			{
+				value: '205623716466262',
+				title: 'Salomon Taiwan',
+				picture:
+					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/306783207_1727285254300093_6976722923823921696_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=4da83f&_nc_ohc=3bGwqDBA-sYAX_1JJKt&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfDgk6evcy2PuCAdUiGF1MfxphZGSaI5pV4JNgMAqbQsCw&oe=65E4850C',
+				link: 'https://www.facebook.com/salomon.tw',
+				network: 'facebook',
+				screenName: 'salomon.tw',
+			},
+			{
+				value: '1375172106064190',
+				title: 'Salomon Philippines',
+				picture:
+					'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-1/305659306_3241841622730553_1272144547067209385_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=4da83f&_nc_ohc=Xyy-i2T1LbcAX_AhOmI&_nc_ht=scontent-sea1-1.xx&edm=AJdBtusEAAAA&oh=00_AfDKjcidH8IuyWV7sj0ZFNjqiXAlX3VDCKQU0RgPLb5ejA&oe=65E349F3',
+				link: 'https://www.facebook.com/salomonphilippines',
+				network: 'facebook',
+				screenName: 'salomonphilippines',
 			},
 		],
 		name: 'cpProfiles',

93259b651500a569c511f714c51e59f9953eab8e
Milan Janoch <milan.janoch@emplifi.io>
2024-03-23 12:51:53 +0100

    removing queryclient

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './components/main-page/main-page'
 import Emplifi from './components/emplifi/emplifi'
 import TourDeFrance from './components/tour-de-france/tour-de-france'
 import TestPage from './components/test-page'
 
-const queryClient = new QueryClient()
-
 const App = () => {
 	return (
 		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<Routes>
-					<Route path='/emplifi' element={<Emplifi />} />
-					<Route path='/tourdefrance' element={<TourDeFrance />} />
-					<Route path='/test' element={<TestPage />} />
-					<Route path='/' element={<MainPage />} />
-				</Routes>
-			</QueryClientProvider>
+			<Routes>
+				<Route path='/emplifi' element={<Emplifi />} />
+				<Route path='/tourdefrance' element={<TourDeFrance />} />
+				<Route path='/test' element={<TestPage />} />
+				<Route path='/' element={<MainPage />} />
+			</Routes>
 		</BrowserRouter>
 	)
 }
